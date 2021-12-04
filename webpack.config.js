3bf9dc30cec4e46aacf7f29f64fd4532e8757fa5
Temirov Ilkom <ilhomtemirov9601@gmail.com>
2021-12-05 00:32:24 +0900

    webpack confuguration

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: ["regenerator-runtime/runtime", "./frontend/main.js"],
+    output: {
+        filename: "main-bundled.js",
+        path: path.resolve(__dirname, "public"),
+    },
+    mode: "production",
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env"],
+                },
+            },
+        }, ],
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
+    },
+};
