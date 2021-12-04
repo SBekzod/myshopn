dc2f344a0c960f661c4d3c0e99e76e1f981e8604
Temirov Ilkom <ilhomtemirov9601@gmail.com>
2021-12-05 00:15:35 +0900

    fix: new project

--- router.js
+++ router.js
@@ -5,28 +5,28 @@ const router = express.Router();
 // create data for our server
 let user;
 fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR: ", err);
-  } else {
-    user = JSON.parse(data);
-  }
+    if (err) {
+        console.log("ERROR: ", err);
+    } else {
+        user = JSON.parse(data);
+    }
 });
 
 router.get("/", (req, res) => {
-  console.log("/");
-  res.end("you are connected");
+    console.log("/");
+    res.end("you are connected");
 });
 router.get("/home", (req, res) => {
-  console.log("/home");
-  res.render("home", { user: user });
+
+    res.render("home", { user: user });
 });
-router.get("/users", (req, res) => {
-  console.log("/users");
-  res.end("you are in users page");
+router.get("/project", (req, res) => {
+
+    res.render("project", { user: user });
 });
 router.get("/shop", (req, res) => {
-  console.log("/shop");
-  res.end("you are in our shop page");
+    console.log("/shop");
+    res.end("you are in our shop page");
 });
 
 module.exports = router;
