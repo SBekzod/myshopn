bba8a82e68a89e63b550a892d2e9a1d1a3c59531
martin <martin@comets.kr>
2021-12-11 13:34:25 +0900

    fix: form on ejs

--- frontend/main.js
+++ frontend/main.js
@@ -1,92 +1,63 @@
 console.log("FRONT END EXECUTED");
 import moment from "moment";
 // import $ from "jquery";
-$(function() {
-    $("#singin").on("click", () => {
-        console.log("click");
-        // window.open("google.com.")
-        OpenPopupCenter("/contact", "my shop", 1000, 600);
+$(function () {
+  $("#singin").on("click", () => {
+    console.log("click");
+    // window.open("google.com.")
+    OpenPopupCenter("/contact", "my shop", 1000, 600);
 
-        $("#library").on("click", () => {
-            console.log("click");
-            OpenPopupCenter("/home", "", 1300, 2000)
-        });
+    $("#library").on("click", () => {
+      console.log("click");
+      OpenPopupCenter("/home", "", 1300, 2000);
     });
+  });
 
+  $("#submit").on("click", () => {
+    console.log("click");
+    const username = $("#username").val();
+    const profession = $("#profession").val();
+    const age = $("#age").val();
+    const adress = $("#adress").val();
+    const password = $("#password").val();
 
-    $("#submit").on("click", () => {
-        console.log("click");
-        const username = $("#username").val();
-        const password = $("#password").val();
-
-        const age = $("#age").val();
-        const adress = $("#adess").val();
-        const place = $("#place").val();
-
-
-        const params = {
-            username: username,
-            password: password,
-            age: age,
-            adress: adress,
-            place: place
-
-
-        }
-        console.log(params)
-        $.post(
-            "/call-me",
-            params,
-            (data) => {
-                console.log("data :", data);
-                window.opener.location.reload();
-                window.close();
-            },
-            "JSON"
-        ).fail((err) => {
-            console.log("ERROR ::: ", err);
-        });
-    });
-
-
-
-
+    const params = {
+      username: username,
+      profession: profession,
+      age: age,
+      adress: adress,
+      password: password,
+    };
+    console.log(params);
+    alert("BUB");
+    // $.post(
+    //     "/call-me",
+    //     params,
+    //     (data) => {
+    //         console.log("data :", data);
+    //         window.opener.location.reload();
+    //         window.close();
+    //     },
+    //     "JSON"
+    // ).fail((err) => {
+    //     console.log("ERROR ::: ", err);
+    // });
+  });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function OpenPopupCenter(pageURL, title, w, h) {
-    let left = Math.round(screen.width / 2 - w / 2);
-    let top = Math.round(screen.height / 2 - h / 2);
-    window.open(
-        pageURL,
-        title,
-        "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=" +
-        w +
-        ", height=" +
-        h +
-        ", top=" +
-        top +
-        ", left=" +
-        left
-    );
+  let left = Math.round(screen.width / 2 - w / 2);
+  let top = Math.round(screen.height / 2 - h / 2);
+  window.open(
+    pageURL,
+    title,
+    "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=" +
+      w +
+      ", height=" +
+      h +
+      ", top=" +
+      top +
+      ", left=" +
+      left
+  );
 }
--- router.js
+++ router.js
@@ -5,31 +5,29 @@ const router = express.Router();
 // create data for our server
 let user;
 fs.readFile("database/user.json", "utf8", (err, data) => {
-    if (err) {
-        console.log("ERROR: ", err);
-    } else {
-        user = JSON.parse(data);
-    }
+  if (err) {
+    console.log("ERROR: ", err);
+  } else {
+    user = JSON.parse(data);
+  }
 });
 
 router.get("/", (req, res) => {
-
-    res.render("project", { user: user });
+  res.render("project", { user: user });
 });
 
 router.get("/home", (req, res) => {
-    res.render("home", { user: user });
+  res.render("home", { user: user });
 });
 
 router.get("/contact", (req, res) => {
-
-    res.render("contact", { user: user });
+  res.render("contact", { user: user });
 });
 router.post("/call-me", (req, res) => {
-    console.log(`req.body ::: `, req.body);
-    const data = req.body;
-    fs.writeFileSync("database/contact_request.json", JSON.stringify(data));
-    res.json({ state: "success" });
+  console.log(`req.body ::: `, req.body);
+  const data = req.body;
+  fs.writeFileSync("database/contact_request.json", JSON.stringify(data));
+  res.json({ state: "success" });
 });
 
 module.exports = router;
